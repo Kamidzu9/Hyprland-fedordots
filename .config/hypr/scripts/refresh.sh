cdfa28465ba6829eb0ed2ba47aebf2dba1a7acc5
Mykhailo Solovey <kamidzu@fedora.fritz.box>
2024-12-16 21:33:17 +0100

    added webp wallpaper extension(dont use it) and added swww-daemon in startup

--- .config/hypr/scripts/refresh.sh
+++ .config/hypr/scripts/refresh.sh
@@ -6,19 +6,23 @@
 _ps=(waybar rofi swaync)
 for _prs in "${_ps[@]}"; do
     if pidof "${_prs}" >/dev/null; then
-        pkill "${_prs}"
+        pkill "${_prs}"   # Gracefully kill the processes
+        sleep 0.1          # Wait for processes to terminate
     fi
 done
 
-sleep 0.2
 # Relaunch waybar
-waybar > /dev/null 2>&1 &
+sleep 0.5
+if ! pidof waybar >/dev/null; then
+    waybar > /dev/null 2>&1 &   # Start waybar if it's not running
+fi
 
-# relaunch swaync
-sleep 0.2
-swaync > /dev/null 2>&1 &
+# Relaunch swaync
+if ! pidof swaync >/dev/null; then
+    swaync > /dev/null 2>&1 &   # Start swaync if it's not running
+fi
 
-# for cava-pywal (note, need to manually restart cava once wallpaper changes)
+# For cava-pywal (note, need to manually restart cava once wallpaper changes)
 ln -sf "$HOME/.cache/wal/cava-colors" "$HOME/.config/cava/config" || true
 
 exit 0
--- .config/hypr/scripts/wallpaperSelect.sh
+++ .config/hypr/scripts/wallpaperSelect.sh
@@ -19,7 +19,7 @@ if pidof swaybg > /dev/null; then
 fi
 
 # Retrieve image files as a list
-PICS=($(find -L "${wallpaperDir}" -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.gif \) | sort ))
+PICS=($(find -L "${wallpaperDir}" -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.gif -o -iname \*.webp \) | sort ))
 
 # Use date variable to increase randomness
 randomNumber=$(( ($(date +%s) + RANDOM) + $$ ))
@@ -37,7 +37,7 @@ executeCommand() {
 
   elif command -v swaybg &>/dev/null; then
     swaybg -i "$1" &
-  
+
   else
     echo "Neither swww nor swaybg are installed."
     exit 1
@@ -53,7 +53,7 @@ menu() {
   printf "$randomChoice\n"
 
   for i in "${!PICS[@]}"; do
-  
+
     # If not *.gif, display
     if [[ -z $(echo "${PICS[$i]}" | grep .gif$) ]]; then
       printf "$(basename "${PICS[$i]}" | cut -d. -f1)\x00icon\x1f${PICS[$i]}\n"
@@ -111,4 +111,3 @@ if pidof rofi > /dev/null; then
 fi
 
 main
-
